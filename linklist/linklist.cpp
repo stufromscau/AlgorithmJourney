65e95f58f0b25ef8d3943788cbc8b7d4a67fb60b
stufromscau <2934095119@qq.com>
2026-05-05 21:56:20 +0800

    2026/5/5

---
+++ linklist/linklist.cpp
@@ -0,0 +1,14 @@
+﻿// linklist.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+typedef struct ListNode {
+    int val;
+    struct ListNode* next;
+};
